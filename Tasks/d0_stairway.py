dcd04c0b6ae487961b8aa7ad1d487b43cb687283
Marlit <m-litv@yandex.ru>
2022-09-14 20:42:46 +0300

    init d1,2

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -9,4 +9,38 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :return: minimal cost of getting to the top
     """
     print(stairway)
-    return 0
+    cost = stairway.copy()
+    for i in range(2, len(stairway)):
+        cost[i] += min(cost[i-1], cost[i-2])
+    return cost[-1]
+
+
+def stairway_path_r(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
+    """
+    Calculate min cost of getting to the top of stairway if agent can go on next or through one step.
+
+    :param stairway: list of ints, where each int is a cost of appropriate step
+    :return: minimal cost of getting to the top
+    """
+    print(stairway)
+
+    cost = [''] * len(stairway)
+    cost[0] = stairway[0]
+    cost[1] = stairway[1]
+
+    def rec(n: int) -> Union[float, int]:
+        if cost[n] != '':
+            return cost[n]
+        if n < 2:
+            return cost[n]
+        cost[n] = min(rec(n - 1) + stairway[n], rec(n-2) + stairway[n])
+        return cost[n]
+
+    return rec(len(stairway) - 1)
+
+
+
+
+
+
+
--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -1,4 +1,4 @@
-def calculate_paths(shape: (int, int), point: (int, int)) -> int:
+def calculate_paths(shape: (int, int), point: (int, int), start_point=(1, 1)) -> int:
     """
     Given field with size rows*cols count available paths from (0, 0) to point
 
@@ -7,4 +7,23 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     :return: count of paths from (1, 1) to (point[0], point[1]) (numerating from 0, so (0, 0) - left bottom tile)
     """
     print(shape, point)
-    return 0
+    table =[[0 for i in range(shape[1])] for i in range(shape[0])]
+    table[start_point[0]][start_point[1]]=1
+
+    def path_rec(i: int, j: int) -> int:
+        if 0 <= i <= shape[0] - 1 and 0 <= j <= shape[1] - 1:
+            if table[i][j]:
+                return table[i][j]
+            # print((i+1, j-2), (i-1, j-2), (i-2, j-1), (i-2, j+1))
+            table[i][j] = sum((path_rec(i+1, j-2),
+                               path_rec(i-1, j-2),
+                               path_rec(i-2, j-1),
+                               path_rec (i-2, j+1),
+                               ))
+            return table[i][j]
+
+        return 0
+
+    return path_rec(*point)
+
+# return 0
